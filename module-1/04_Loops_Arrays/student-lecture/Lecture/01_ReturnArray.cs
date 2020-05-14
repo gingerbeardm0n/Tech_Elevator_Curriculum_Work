0c18cb57e12e0f3aadb0d2b8fc47b307340e7e86
John Fulton <john@techelevator.com>
2020-05-14 14:17:46 +0000

    01_ReturnArray.cs edited online with Bitbucket done as a test of git conflict

--- module-1/04_Loops_Arrays/student-lecture/Lecture/01_ReturnArray.cs
+++ module-1/04_Loops_Arrays/student-lecture/Lecture/01_ReturnArray.cs
@@ -17,6 +17,9 @@ namespace Lecture
         */
         public int[] ReturnNewArray()
         {
+            // this is a chenge done on the server
+            int result = 0;
+            
             return null;
         }
 
