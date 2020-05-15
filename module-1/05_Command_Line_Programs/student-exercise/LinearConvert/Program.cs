7db050efb36eee5475001e04145708656a79f3cf
Joel Birdsall <joel.birdsall@gmail.com>
2020-05-15 17:28:23 -0400

    Module 1 Day 5 1st commit LinearConversion complete

--- module-1/05_Command_Line_Programs/student-exercise/LinearConvert/Program.cs
+++ module-1/05_Command_Line_Programs/student-exercise/LinearConvert/Program.cs
@@ -4,9 +4,47 @@ namespace LinearConvert
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double finalLength = 0;
+            double convertedFeet = 0;
+            double convertedMeters = 0;
+
+            
+            Console.WriteLine("Please enter a length!");
+            string userInput = Console.ReadLine();
+            double length = double.Parse(userInput);
+
+            Console.WriteLine("Please specify if it's meters or feet (M/F)");
+            string measurmentType = Console.ReadLine();
+            
+
+            switch (measurmentType)
+            {
+                case "M":
+                case "m":
+                    convertedFeet = length * 3.2808399;
+                    Console.WriteLine("The input was " + length + " and the ouput is " + convertedFeet);
+                    break;
+                case "F":
+                case "f":
+                    
+                    convertedMeters = length * 0.3048;
+                    Console.WriteLine("The input was " + length + " and the ouput is " + convertedMeters);
+                    break;
+                default:
+                    Console.WriteLine("you dumbass, try again");
+                    break;
+            }
+
+            Console.WriteLine();
+
+            Console.ReadLine();
         }
+
+            
     }
 }
