322dc555ecb6ad978f8d706dee2faa83dc43ba18
John Fulton <john@techelevator.com>
2020-05-18 13:21:24 -0400

    module-1/06_Introduction_Objects_Strings/ in-class changes

---
+++ module-1/06_Introduction_Objects_Strings/lecture-final/ClassesAndObjects/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dogAges = new int[4];
+
+            Television myTelevision = new Television();
+
+            myTelevision.model = "Sony";
+            myTelevision.screenSize = 10;
+
+            myTelevision.PrintInfo();
+
+            Television lisasTelevision = new Television();
+            lisasTelevision.model = "Sony";
+            lisasTelevision.screenSize = 56;
+            lisasTelevision.PrintInfo();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ module-1/06_Introduction_Objects_Strings/lecture-final/ClassesAndObjects/Television.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesAndObjects
+{
+    public class Television
+    {
+        public int screenSize = 0;
+        public string model = "";
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("Screen size is " + screenSize + " inches");
+            Console.WriteLine("Model number is " + model);
+        }
+    }
+}
