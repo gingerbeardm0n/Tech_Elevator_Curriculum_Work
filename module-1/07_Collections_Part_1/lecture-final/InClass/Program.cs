732413f059ab1513b3307fed0bcbc726428a6c4a
John Fulton <john@techelevator.com>
2020-05-19 20:01:23 -0400

    module-1/07_Collections_Part_1/ in-class changes

---
+++ module-1/07_Collections_Part_1/lecture-final/InClass/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<int> dogAges = new List<int>();
+
+            dogAges.Add(3);
+            dogAges.Add(4);
+
+            dogAges.Reverse();
+
+            foreach(int item in dogAges)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
