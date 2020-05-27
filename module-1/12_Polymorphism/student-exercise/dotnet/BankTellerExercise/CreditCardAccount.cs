e043fa9bb39970c41dc94045b8db6bb16e92fbd7
Joel Birdsall <joel.birdsall@gmail.com>
2020-05-27 14:59:26 -0400

    M1 D12 PolyDeathism 3/4 complete

---
+++ module-1/12_Polymorphism/student-exercise/dotnet/BankTellerExercise/CreditCardAccount.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTellerExercise
+{
+    public class CreditCardAccount : IAccountable
+    {
+
+        public string AccountHolderName { get; private set; }
+        public string AccountNumber { get; private set; }
+        public int Debt { get; private set; } 
+        
+            
+        public int Balance { get; }
+
+        //-----CONSTRUCTORS---------------------------------------------------------------------------------------------
+
+        public CreditCardAccount(string accountHolderName, string accountNumber)
+        {
+            AccountHolderName = accountHolderName;
+            AccountNumber = accountNumber;
+            Debt = 0;
+        }
+
+        //-----METHODS--------------------------------------------------------------------------------------------------
+
+        public int Pay(int amountToPay)
+        {
+            Debt = Debt - amountToPay;
+            
+            return Debt;
+        }
+        public int Charge(int amountToCharge)
+        {
+            Debt += amountToCharge;
+            return Debt;
+        }
+
+    }
+}
