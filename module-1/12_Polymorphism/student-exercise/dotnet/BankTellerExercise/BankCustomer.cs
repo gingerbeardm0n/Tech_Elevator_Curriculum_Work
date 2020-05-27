4295284a8fb5e58667b56828f79993ac771c0ea5
Joel Birdsall <joel.birdsall@gmail.com>
2020-05-27 17:23:03 -0400

    M1 D12 just a wee bit more done

--- module-1/12_Polymorphism/student-exercise/dotnet/BankTellerExercise/BankCustomer.cs
+++ module-1/12_Polymorphism/student-exercise/dotnet/BankTellerExercise/BankCustomer.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace BankTellerExercise
 {
-    class BankCustomer
+    class BankCustomer : IAccountable
     {
+        List<IAccountable> bankAccounts = new List<IAccountable>();
+
+
+
+
+
+        public int Balance { get; private set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool IsVip
+        {
+            get
+            {
+                int totalBalance = 0;
+                foreach (IAccountable individualBalance in bankAccounts)
+                {
+                    totalBalance += individualBalance.Balance;
+                }
+                if (totalBalance >= 25000)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+
+
+
+
     }
 }
