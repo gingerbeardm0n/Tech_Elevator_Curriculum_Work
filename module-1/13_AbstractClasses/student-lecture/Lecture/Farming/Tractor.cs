1d8ffc862f781df0f4d78b45981113cc38f8d5ed
Matt Eland <matt.eland@gmail.com>
2020-05-27 16:04:50 -0400

    module-1/13_AbstractClasses

---
+++ module-1/13_AbstractClasses/student-lecture/Lecture/Farming/Tractor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class Tractor : ISingAbout
+    {
+        public string Name
+        {
+            get
+            {
+                return "Tractor";
+            }
+        }
+
+        public string MakeSoundOnce()
+        {
+            return "Vruuuuuum (sputter) / rumble";
+        }
+
+        public string MakeSoundTwice()
+        {
+            return "VROOOOOOOOOOOOOOOOOOOOOOM! (boom boom)";
+        }
+    }
+}
---
+++ module-1/13_AbstractClasses/student-lecture/Lecture/Program.cs
@@ -0,0 +1,70 @@
+﻿using Lecture.Farming;
+using System;
+using System.Collections.Generic;
+
+namespace Lecture
+{
+    class Program
+    {
+        private static void SingAbout(ISingAbout animal)
+        {
+            Console.WriteLine("Old MacDonald had a farm ee ay ee ay oh");
+            Console.WriteLine("And on his farm there was a " + animal.Name + " ee ay ee ay oh");
+            Console.WriteLine("With a " + animal.MakeSoundTwice() + " here and a " + animal.MakeSoundTwice() + " there");
+            Console.WriteLine("Here a " + animal.MakeSoundOnce() + ", there a " + animal.MakeSoundOnce() + " everywhere a " + animal.MakeSoundTwice());
+            Console.WriteLine("Old Macdonald had a farm, ee ay ee ay oh");
+            Console.WriteLine();
+        }
+
+        static IFlyable GetFlyingThing()
+        {
+            return new Dragon();
+        }
+
+        static void Main(string[] args)
+        {
+            //
+            // OLD MACDONALD
+            //
+
+            List<ISingAbout> animals = new List<ISingAbout>();
+            animals.Add(new Horse());
+            animals.Add(new Elephant());
+            animals.Add(new Dragon());
+            animals.Add(new Tractor());
+
+            FarmAnimal flyingThing = new Dragon();
+
+            IFlyable trogdor = GetFlyingThing();
+
+
+            foreach (ISingAbout item in animals)
+            {
+                SingAbout(item);
+            }
+
+            /*
+
+            // Let's try singing about a Farm Animal
+            FarmAnimal animal = new Horse();
+            SingAbout(animal);
+
+            // Horse horse = (Horse)animal;
+            // horse.Gallop();
+
+            Elephant elephant = new Elephant();
+            SingAbout(elephant);
+
+            Dragon dragon = new Dragon();
+            SingAbout(dragon);
+            // Same thing as: SingAbout(new Dragon());
+            */
+
+            // What if we wanted to sing about other things on the farm that were 
+            // singable but not farm animals?
+            // Can it be done? 
+
+            Console.ReadLine();
+        }
+    }
+}
