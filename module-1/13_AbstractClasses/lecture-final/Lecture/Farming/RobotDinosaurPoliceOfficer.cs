0945085f2ec78408ec808b3e44b4ac4c8cfb95a5
Matt Eland <matt.eland@gmail.com>
2020-05-28 14:35:55 -0400

    Final lecture code from today.

---
+++ module-1/13_AbstractClasses/lecture-final/Lecture/Farming/RobotDinosaurPoliceOfficer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lecture.Farming
+{
+    public class RobotDinosaurPoliceOfficer : FlyingFarmAnimal
+    {
+        public override string Name
+        {
+            get
+            {
+                return "RoboCoptor";
+            }
+        }
+
+        protected override string MakeSound()
+        {
+            base.GiveScritches();
+
+            return "Beep Boop Roar. I am ze law";
+        }
+
+        public override void PoopOnPedestrian()
+        {
+            // Best not to think about this one...
+        }
+    }
+}
