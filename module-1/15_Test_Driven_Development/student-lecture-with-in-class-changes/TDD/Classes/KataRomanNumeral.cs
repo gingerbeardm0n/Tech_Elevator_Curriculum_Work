002480f563fafa0691e3864eea0074d44a76cde0
John Fulton <john@techelevator.com>
2020-06-01 12:35:33 -0400

    module-1/15_Test_Driven_Development/ in-class changes

---
+++ module-1/15_Test_Driven_Development/student-lecture-with-in-class-changes/TDD/Classes/KataRomanNumeral.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD.Classes
+{
+    public class KataRomanNumeral
+    {
+       public string ConvertToRomanNumeral(int n)
+        {
+            string result = "";
+
+            result += ConvertDigit(ref n, 1000, "M");
+            result += ConvertDigit(ref n, 500, "D");
+            result += ConvertDigit(ref n, 100, "C");
+            result += ConvertDigit(ref n, 50, "L");
+            result += ConvertDigit(ref n, 10, "X");
+            result += ConvertDigit(ref n, 5, "V");
+            result += ConvertDigit(ref n, 1, "I");
+
+            result = result.Replace("LXXXX", "XC");
+            result = result.Replace("XXXX", "XL");
+            result = result.Replace("VIIII", "IX");
+            result = result.Replace("IIII", "IV");
+
+            return result;
+        }
+
+        private string ConvertDigit(ref int n, int target, string roman)
+        {
+            string result = "";
+            while (n >= target)
+            {
+                result += roman;
+                n -= target;
+            }
+
+            return result;
+        }
+    }
+}
