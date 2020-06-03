155a9c851c30a4d5266b467cb90743ee0dddb2a4
John Fulton <john@techelevator.com>
2020-06-03 13:49:27 -0400

    module-1/17_FileIO_Writing_out/ in-class changes

---
+++ module-1/17_FileIO_Writing_out/PetInfoV2-with-Johns-changes/PetInfo/FileAccess.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PetInfo
+{
+    public class FileAccess
+    {
+        private string directory = @"c:\petinfo";
+        private string filename = @"petinfo.csv";
+
+        public List<Pet> ReadPets()
+        {
+            List<Pet> pets = new List<Pet>();
+
+            string fullPath = Path.Combine(directory, filename);
+            try
+            {
+                using (StreamReader sr = new StreamReader(fullPath))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string unsplit = sr.ReadLine();
+                        string[] split = unsplit.Split('|');
+
+                        Pet temp = new Pet();
+                        temp.Number = int.Parse(split[0]);
+                        temp.Name = split[1];
+                        temp.FamilyName = split[2];
+                        temp.PetType = split[3];
+
+                        pets.Add(temp);
+                    }
+                }
+            }
+            catch
+            {
+                pets = new List<Pet>();
+            }
+
+            return pets;
+        }
+
+        // replaces currenct contents of file with collection
+        public bool WritePets(List<Pet> pets)
+        {
+            bool result = false;
+            string fullPath = Path.Combine(directory, filename);
+
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(fullPath, false))
+                {
+                    foreach (Pet pet in pets)
+                    {
+                        string petString = pet.Number + "|";
+                        petString += pet.Name + "|";
+                        petString += pet.FamilyName + "|";
+                        petString += pet.PetType;
+                        sw.WriteLine(petString);
+                    }
+                }
+
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ module-1/17_FileIO_Writing_out/PetInfoV2-with-Johns-changes/PetInfo/Pets.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PetInfo
+{
+    public class Pets
+    {
+        private List<Pet> pets = new List<Pet>();
+
+        public Pets()
+        {
+            pets = ReadPets();
+        }
+
+        public Pet[] PetList
+        {
+            get
+            {
+                return pets.ToArray();
+            } 
+        }
+
+        // add to pet collection and write pet collection to file
+        public bool AddPet(Pet pet)
+        {
+            pets.Add(pet);
+            bool result = WritePets(pets);
+            return result;
+        }
+
+        // remove pet from collection and write to file
+        public bool RemovePet(int petNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        //read pet from file
+        private List<Pet> ReadPets()
+        {
+            FileAccess fa =new FileAccess();
+            return fa.ReadPets();
+        }
+
+        // write pets to file
+        public bool WritePets(List<Pet> pets)
+        {
+            FileAccess fa = new FileAccess();
+            return fa.WritePets(pets);
+        }
+    }
+}
