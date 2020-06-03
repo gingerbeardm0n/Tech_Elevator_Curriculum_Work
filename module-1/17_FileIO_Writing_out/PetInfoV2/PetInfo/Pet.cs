49994011aa9bcca3590f96deaa1484624a45f45b
John Fulton <john@techelevator.com>
2020-06-03 07:47:55 -0400

    module-1/17_FileIO_Writing_out/ PetInfoV2

---
+++ module-1/17_FileIO_Writing_out/PetInfoV2/PetInfo/Pet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetInfo
+{
+    public class Pet
+    {
+        public Pet(int number, string name,
+            string familyName, string petType)
+        {
+            Number = number;
+            Name = name;
+            FamilyName = familyName;
+            PetType = petType;
+        }
+
+        public Pet()
+        {
+
+        }
+
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public string PetType { get; set; }
+
+        public override string ToString()
+        {
+            return Number.ToString().PadRight(5) + 
+                Name.PadRight(10) + FamilyName.PadRight(10) + PetType.PadRight(10);
+        }
+    }
+}
