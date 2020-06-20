21dd150d59856a177e13bf70579cbdf3271d46d0
John Fulton <john@techelevator.com>
2020-06-20 11:25:09 -0400

    module-3/01-HTTP-Web-Services-GET/ tutorials

---
+++ module-3/01-HTTP-Web-Services-GET/student-tutorial/locations/LocationApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using RestSharp;
+
+namespace LocationApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserInterface ui = new UserInterface();
+            ui.Run();
+        }
+       
+    }
+}
