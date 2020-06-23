8bcae6f453d578bc96c33ca497f4a48f004c9257
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-22 21:19:55 -0400

    Cant believe Im still finishing up Module 1 assignments

--- module-1/14_UnitTesting/student-exercise/dotnet/Exercises.Tests/WordCountTests.cs
+++ module-1/14_UnitTesting/student-exercise/dotnet/Exercises.Tests/WordCountTests.cs
@@ -3,39 +3,34 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace Exercises.Tests
 {
     [TestClass]
     public class WordCountTests
     {
-        //WordCount testObj;
-
-        //[TestInitialize]
-        //public void Init()
-        //{
-        //    testObj = new WordCount();
-        //}
-
+       
         [TestMethod]
-        //[DataRow(new string[] { "ba", "ba", "black", "sheep" }, new Dictionary<string, int> { "ba" : 2, "black": 1, "sheep": 1 })]
-
-        public void PassInExampleFromProblem()
+      
+        public void WordCountTest_IsTheBaneOfMyExistence()
         {
             //------ Arrange -------------------------------------------------------
 
             WordCount mattIsTheJavaBatMan = new WordCount();
-            Dictionary<string, int> mattsSneakyDictionary1 = new Dictionary<string, int>();
+            Dictionary<string, int> mattsSneakyDictionary = new Dictionary<string, int>();
 
             //----Act-----------------------------------------------
             //new string[] words = null;
             
-            mattsSneakyDictionary1 = mattIsTheJavaBatMan.GetCount(new string[]{"ba", "ba", "black", "sheep" });
-            
+            mattsSneakyDictionary = mattIsTheJavaBatMan.GetCount(new string[]{"ba", "ba", "black", "sheep" });
+
             //----Assert-------------------------------------------
-
-            //Assert.ReferenceEquals((new Dictionary<string, int> { "ba" : 2, "black": 1, "sheep": 1 }), mattsSneakyDictionary1);
-
-
+            
+            Assert.AreEqual(2, mattsSneakyDictionary["ba"]);
+            Assert.AreEqual(1, mattsSneakyDictionary["black"]);
+            Assert.AreEqual(1, mattsSneakyDictionary["sheep"]);
+            Assert.AreEqual(3, mattsSneakyDictionary.Count);
+            
         }
     }
 }
