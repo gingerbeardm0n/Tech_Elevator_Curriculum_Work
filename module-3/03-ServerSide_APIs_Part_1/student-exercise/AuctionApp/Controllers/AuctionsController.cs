a74d6323291c6ee0151b2ddef3fab275407c627e
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-24 11:21:08 -0400

    M3-D3 Server side APIs part 1, HW mostly done

--- module-3/03-ServerSide_APIs_Part_1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
+++ module-3/03-ServerSide_APIs_Part_1/student-exercise/AuctionApp/Controllers/AuctionsController.cs
@@ -19,6 +19,54 @@ namespace AuctionApp.Controllers
                 _dao = auctionDao;
         }
 
+        [HttpGet]
+        public List<Auction> List(string title_like = "", double currentBid_lte = 0)
+        {
+
+            if (title_like != null)
+            {
+                return _dao.SearchByTitle(title_like);
+
+            }
+            else if (title_like == null)
+            {
+                return _dao.List();
+            }
+
+            if (currentBid_lte > 0)
+            {
+                return _dao.SearchByPrice(currentBid_lte);
+            }
+            else if (currentBid_lte <= 0)
+            {
+                return _dao.List();
+            }
+            return _dao.List();
+        }
+
+
+    [HttpGet("{id}")]
+    public Auction Get(int id)
+    {
+        return _dao.Get(id);
+    }
+
+    [HttpPost]
+    public Auction Create(Auction inputAuction)
+    {
+        Auction specificAuction = _dao.Create(inputAuction);
+
+        if (specificAuction.IsValid == true)
+        {
+            return specificAuction;
+        }
+        return null;
+    }
+
 
     }
+
+
+
 }
+
