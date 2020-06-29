27ec3d826d56d9e88d70e2f9e62e51456695ce57
Joel Birdsall <joel.birdsall@gmail.com>
2020-06-29 08:29:21 -0400

    Tutorial done

--- module-3/06-Authentication/student-tutorial-improved/LocationsClient/APIService.cs
+++ module-3/06-Authentication/student-tutorial-improved/LocationsClient/APIService.cs
@@ -10,14 +10,20 @@ namespace Locations
         const string API_BASE = "https://localhost:44387";
         const string API_URL = API_BASE + "/locations";
         private API_User user = new API_User();
+        private static RestClient authClient = new RestClient();
 
         public bool LoggedIn { get { return !string.IsNullOrWhiteSpace(user.Token); } }
 
         public List<Location> GetAllLocations()
         {
-            RestClient client = new RestClient();
+            //RestClient client = new RestClient();
+            //if (!string.IsNullOrEmpty(user.Token))
+            //{
+            //    JwtAuthenticator jwt = new JwtAuthenticator(user.Token);
+            //    client.Authenticator = jwt;
+            //}
             RestRequest request = new RestRequest(API_URL);
-            IRestResponse<List<Location>> response = client.Get<List<Location>>(request);
+            IRestResponse<List<Location>> response = authClient.Get<List<Location>>(request);
 
             if (response.ResponseStatus != ResponseStatus.Completed)
             {
@@ -38,9 +44,14 @@ namespace Locations
 
         public Location GetDetailsForLocation(int locationId)
         {
-            RestClient client = new RestClient();
+            //RestClient client = new RestClient();
+            //if (!string.IsNullOrEmpty(user.Token))
+            //{
+            //    JwtAuthenticator jwt = new JwtAuthenticator(user.Token);
+            //    client.Authenticator = jwt;
+            //}
             RestRequest requestOne = new RestRequest(API_URL + "/" + locationId);
-            IRestResponse<Location> response = client.Get<Location>(requestOne);
+            IRestResponse<Location> response = authClient.Get<Location>(requestOne);
 
             if (response.ResponseStatus != ResponseStatus.Completed)
             {
@@ -64,7 +75,7 @@ namespace Locations
             RestClient client = new RestClient();
             RestRequest request = new RestRequest(API_URL);
             request.AddJsonBody(newLocation);
-            IRestResponse<Location> response = client.Post<Location>(request);
+            IRestResponse<Location> response = authClient.Post<Location>(request);
 
             if (response.ResponseStatus != ResponseStatus.Completed)
             {
@@ -87,7 +98,7 @@ namespace Locations
             RestClient client = new RestClient();
             RestRequest request = new RestRequest(API_URL + "/" + locationToUpdate.Id);
             request.AddJsonBody(locationToUpdate);
-            IRestResponse<Location> response = client.Put<Location>(request);
+            IRestResponse<Location> response = authClient.Put<Location>(request);
 
             if (response.ResponseStatus != ResponseStatus.Completed)
             {
@@ -110,7 +121,7 @@ namespace Locations
         {
             RestClient client = new RestClient();
             RestRequest request = new RestRequest(API_URL + "/" + locationId);
-            IRestResponse response = client.Delete(request);
+            IRestResponse response = authClient.Delete(request);
 
             if (response.ResponseStatus != ResponseStatus.Completed)
             {
@@ -150,6 +161,7 @@ namespace Locations
             else
             {
                 user.Token = response.Data.Token;
+                authClient.Authenticator = new JwtAuthenticator(user.Token);
 
                 return true;
             }
