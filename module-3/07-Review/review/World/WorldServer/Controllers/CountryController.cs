6ec3f08803759cac3d129e441251511dbaaf8c67
John Fulton <john@techelevator.com>
2020-06-30 05:16:06 -0400

    module-3/07-Review/  update

--- module-3/07-Review/review/World/WorldServer/Controllers/CountryController.cs
+++ module-3/07-Review/review/World/WorldServer/Controllers/CountryController.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using WorldServer.APIServices;
 using WorldServer.DAL;
 using WorldServer.Models;
+using System.Web;
 
 namespace WorldServer.Controllers
 {
@@ -26,6 +27,8 @@ namespace WorldServer.Controllers
         [HttpGet]
         public List<Country> GetCountries()
         {
+            var user = User.Identity.Name;
+
             List<Country> countries = countryDAO.GetCountries();
             return countries;
         }
@@ -34,6 +37,25 @@ namespace WorldServer.Controllers
         [HttpGet("{countryCode}")]
         public CountryDetail GetCountry(string countryCode)
         {
+            //owner?
+            // https://docs.microsoft.com/en-us/dotnet/api/system.web.httpcontext.user?view=netframework-4.8
+            // https://docs.microsoft.com/en-us/dotnet/api/system.security.claims.claim?view=netcore-3.1
+
+            var user = User.Identity.Name;
+            int id = -1;
+
+            foreach (var claim in User.Claims)
+            {
+                if (claim.Type == "sub")
+                {
+                    id = int.Parse(claim.Value);
+                }
+            }
+
+            System.Diagnostics.Debug.WriteLine("".PadRight(20,'-') + " from GetCounty " + "".PadRight(20, '-'));
+            System.Diagnostics.Debug.WriteLine("User: " + user);
+            System.Diagnostics.Debug.WriteLine("User id: " + id);
+
             Country country = countryDAO.GetCountry(countryCode);
 
             CountryDetail countryDetail = new CountryDetail(country);
