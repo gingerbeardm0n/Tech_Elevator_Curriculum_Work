81e7a9fa1ad7073214b1b0254e27fec4147677f9
Matt Eland <matt.eland@gmail.com>
2020-07-14 15:32:56 -0400

    module-4/22_DOM - Better styles and comments

--- module-4/22_DOM/student-lecture/assets/js/questions.js
+++ module-4/22_DOM/student-lecture/assets/js/questions.js
@@ -39,19 +39,44 @@ const questions = [
   }
 ];
 
-
-
-
+// This function is called when the DOM is ready to interact with
+// Don't worry about this syntax for today, but this will be relevant tomorrow. 
 document.addEventListener('DOMContentLoaded', () => {
+
   console.log('Hello from Questions.js');
 
+  // (PART 1 - Querying the DOM)
   // TODO: Find the question container (questionContainer class or divQuestions id)
-
+  
   // TODO: Log the question container to the console
 
-  // TODO: Set the InnerText and InnerHTML of the question container
+  // (PART 2 - DOM Manipulation)
 
-  // TODO: Loop over questions and add questions to the container
+  // TODO: Set the InnerHTML and InnerText of the question container
 
-  // TODO: Style our questions using a template
+  // TODO: Add the 'ugly' class to the question container
+
+  // (PART 3 - Adding to the DOM)
+  // TODO: Loop over the questions array
+  {
+    // TODO: Create a new h1 element
+
+    // TODO: Append this element as a child element to the document
+
+    // TODO: Add a class of 'question-header' to each element we create
+
+    // (PART 4 - Templates)
+
+    // TODO: Get a reference to the template in the document (ideally, this should be done once outside the loop)
+
+    // TODO: Clone the template and get its content
+
+    // TODO: Using insertAdjacentElement, add the template to the container
+  }
+
+  // TODO: Using document.querySelectorAll, query all articles that were created 
+  
+  // TODO: Add the "blur" class to each element that is created
+  // (NOTE: we should do this when creating them earlier, not after the fact via a query - this is just for teaching querySelectorAll)
+
 });
