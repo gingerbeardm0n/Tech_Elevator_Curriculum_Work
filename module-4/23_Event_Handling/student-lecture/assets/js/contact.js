c3fa8c99c43c2d854b9c5d799c39efe472a9a8cd
Matt Eland <matt.eland@gmail.com>
2020-07-15 23:42:15 -0400

    module-4/23_Event_Handling - Lecture ready for dry run

--- module-4/23_Event_Handling/student-lecture/assets/js/contact.js
+++ module-4/23_Event_Handling/student-lecture/assets/js/contact.js
@@ -1,3 +1,7 @@
+function respondToBlur(event) {
+    console.log('A blur occurred. The last value was ' + event.target.value);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     // Submit Button has an id of btnSubmit
     // Form has an id of formContact
@@ -6,4 +10,11 @@ document.addEventListener('DOMContentLoaded', () => {
     // TODO: Display a message to the user
 
     // TODO: Stop the browser from navigating
+
+    // Get all form elements and test what happens when we do blurs
+    const allInputs = document.querySelectorAll('input');
+    allInputs.forEach(element => {
+        // TODO: Add listeners to blur events
+    });
+
 });
--- module-4/23_Event_Handling/student-lecture/pre-baked/contact.js
+++ module-4/23_Event_Handling/student-lecture/pre-baked/contact.js
@@ -1,5 +1,11 @@
-// TODO: listen to DOMContentLoaded
+function respondToBlur(event) {
+    console.log('A blur occurred. The last value was ' + event.target.value);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    // Submit Button has an id of btnSubmit
+    // Form has an id of formContact
+    // TODO: Try to react to the user submitting the form
     const form = document.getElementById('formContact');
     form.addEventListener('submit', (event) => {
         console.log(event);
@@ -7,4 +13,18 @@ document.addEventListener('DOMContentLoaded', () => {
 
         event.preventDefault();
     });
+
+    // TODO: Display a message to the user
+
+    // TODO: Stop the browser from navigating
+
+    // Get all form elements
+    const allInputs = document.querySelectorAll('input');
+    allInputs.forEach(element => {
+        // TODO: Add listeners to blur events
+        element.addEventListener('blur', respondToBlur);
+
+        element.removeEventListener('blur', respondToBlur);
+    });
+
 });
