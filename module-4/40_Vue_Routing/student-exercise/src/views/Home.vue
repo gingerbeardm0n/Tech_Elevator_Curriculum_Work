646cd532fed254b16ec3678b58f42a85e88d5f6b
Joel Birdsall <joel.birdsall@gmail.com>
2020-07-28 18:09:13 -0400

    vue router HW half done

---
+++ module-4/40_Vue_Routing/student-exercise/src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <bestseller-list />
+      <new-releases-list />
+  </div>
+</template>
+
+<script>
+import BestsellerList from '../components/BestsellerList.vue'
+import NewReleasesList from '../components/NewReleasesList.vue'
+
+export default {
+    components: {
+        BestsellerList,
+        NewReleasesList
+    }
+}
+</script>
+
+<style>
+
+</style>
